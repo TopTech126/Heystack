c6d5e808c065cc3f3e9faa0e53d4180ae7058076
smartstandard <smartcode2414@gmail.com>
2024-12-27 02:48:09 +0900

    feat:connect links

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,6 +1,7 @@
 const navbarToggle = document.querySelector('.navbar-toggle');
 const modalClose = document.querySelector('.modal-close');
 const modal = document.querySelector('.modal');
+const navLinks = document.querySelectorAll('.modal-nav a');
 
 navbarToggle.addEventListener('click', () => {
   modal.classList.add('show');
@@ -14,6 +15,16 @@ modalClose.addEventListener('click', () => {
   setTimeout(() => {
     modal.style.display = 'none';
   }, 300);
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    setTimeout(() => {
+      modal.style.display = 'none';
+    }, 300);
+  });
 });
 
 window.addEventListener('click', (event) => {
