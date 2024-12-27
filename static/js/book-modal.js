b4d9077c94c8b6a46385ad9b8ab5c5a97bcf128d
smartstandard <smartcode2414@gmail.com>
2024-12-28 04:35:26 +0900

    feat:see live in action modal

---
+++ static/js/book-modal.js
@@ -0,0 +1,22 @@
+const bookModal = document.getElementById("book-modal");
+const btn = document.getElementById("open-book-modal");
+const btn2 = document.getElementById("open2-book-modal");
+const span = document.getElementsByClassName("book-close-button")[0];
+
+btn.onclick = function() {
+  bookModal.style.display = "block";
+}
+
+btn2.onclick = function() {
+  bookModal.style.display = "block";
+}
+
+span.onclick = function() {
+  bookModal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    bookModal.style.display = "none";
+  }
+}
