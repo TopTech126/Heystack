4dc72470669b5defb9f1e1070caaf47d1985d469
smartstandard <smartcode2414@gmail.com>
2025-01-03 23:59:03 +0900

    feat:email-function job-bid, book

--- static/js/apply-modal.js
+++ static/js/apply-modal.js
@@ -2,20 +2,22 @@ const applyModal = document.getElementById("apply-modal");
 const openApply = document.getElementsByClassName("open-apply");
 const closeApply = document.getElementsByClassName("close-apply")[0];
 
-for(let i = 0; i < openApply.length; i++){
-  openApply[i].addEventListener("click", function(e){
+for (let i = 0; i < openApply.length; i++) {
+  openApply[i].addEventListener("click", function (e) {
     applyModal.style.display = "block";
   })
 }
-if(closeApply){
-  closeApply.onclick = function() {
+
+if (closeApply) {
+  closeApply.onclick = function () {
     applyModal.style.display = "none";
   }
 }
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == applyModal) {
-    if(applyModal)
+    if (applyModal)
       applyModal.style.display = "none";
   }
 }
+
---
+++ static/js/book-email.js
@@ -0,0 +1,25 @@
+(function () {
+    emailjs.init("AxfWH57xCUpbKEHPO");
+})();
+
+document.getElementById('book-email-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const templateParams = {
+        to: 'saveearthdev@gmail.com',
+        subject: "Book",
+        name: document.getElementById('book-name').value,
+        email: document.getElementById('book-email').value,
+        phone: document.getElementById('book-subject').value,
+        message: document.getElementById('book-message').value,
+    };
+
+    emailjs.send('service_l3gukc9', 'template_n2dk8vg', templateParams)
+        .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+            alert('Email sent successfully!');
+        }, function (error) {
+            console.log('FAILED...', error);
+            alert('Email sending failed. Please try again later.');
+        });
+});
