4aa5305b4486125fd4e865cea0b9136eeaab3eff
smartstandard <smartcode2414@gmail.com>
2025-01-03 14:53:40 +0900

    feat:apply-modal

---
+++ static/js/apply-modal.js
@@ -0,0 +1,21 @@
+const applyModal = document.getElementById("apply-modal");
+const openApply = document.getElementsByClassName("open-apply");
+const closeApply = document.getElementsByClassName("close-apply")[0];
+
+for(let i = 0; i < openApply.length; i++){
+  openApply[i].addEventListener("click", function(e){
+    applyModal.style.display = "block";
+  })
+}
+if(closeApply){
+  closeApply.onclick = function() {
+    applyModal.style.display = "none";
+  }
+}
+
+window.onclick = function(event) {
+  if (event.target == applyModal) {
+    if(applyModal)
+      applyModal.style.display = "none";
+  }
+}
