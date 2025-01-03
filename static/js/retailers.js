78270edb6a126945fb15e81d6e4efe126762ac36
smartstandard <smartcode2414@gmail.com>
2025-01-04 04:09:03 +0900

    style:retailer

--- static/js/retailers.js
+++ static/js/retailers.js
@@ -1,10 +1,50 @@
+function getBreakpoints() {
+  const screenWidth = window.innerWidth;
+
+  if (screenWidth < 320) {
+    return {
+      0: {
+        slidesPerView: 1,
+      },
+    };
+  } else if (screenWidth < 640) {
+    return {
+      0: {
+        slidesPerView: 1,
+      },
+      200: {
+        slidesPerView: 2,
+      },
+    };
+  } else {
+    return {
+      0: {
+        slidesPerView: 1,
+      },
+      200: {
+        slidesPerView: 2,
+      },
+      600: {
+        slidesPerView: 4,
+      },
+      800: {
+        slidesPerView: 5,
+      },
+      1000: {
+        slidesPerView: 6,
+      },
+    };
+  }
+}
+
+
 var swiper = new Swiper(".retailers-content", {
-  slidesPerView: 1,
+  slidesPerView: 6,
   spaceBetween: 10,
   loop: true,
-  centerSlide: 'true',
-  fade: 'true',
-  grabCursor: 'true',
+  centerSlide: true,
+  fade: true,
+  grabCursor: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -14,25 +54,11 @@ var swiper = new Swiper(".retailers-content", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  breakpoints: getBreakpoints(),
+});
 
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    200: {
-      slidesPerView: 2,
-    },
-    400: {
-      slidesPerView: 3,
-    },
-    600: {
-      slidesPerView: 4,
-    },
-    800: {
-      slidesPerView: 5,
-    },
-    1000: {
-      slidesPerView: 6,
-    },
-  },
+
+window.addEventListener('resize', function () {
+  swiper.params.breakpoints = getBreakpoints();
+  swiper.update();
 });
